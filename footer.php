a90fa830c680c699b404b8f7ba4d86102c377cc2
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-02-26 17:24:58 +0200

    adds missing links in the footer

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		&copy; <?php echo date_i18n(_x('Y', 'copyright date format', 'matter')); ?>
 	</div>
 	<div class="footer-credits">
-		<span class="pr1 mv1"><a href="">Institute of Technology</a>,&nbsp;<a href="">University of Tartu</a>. Funded by the European Union</span>
+		<span class="pr1 mv1"><a href="https://www.tuit.ut.ee/en">Institute of Technology</a>,&nbsp;<a href="https://www.ut.ee/en">University of Tartu</a>. Funded by the European Union</span>
 		<img class="eu-flag" src="<?php echo get_template_directory_uri(); ?>/assets/images/flag_yellow_low.jpg" />
 	</div>
 </footer>
